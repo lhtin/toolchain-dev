fff655066693f2d29070dbf714dd089262d9d615
Lehua Ding <lehua.ding@rivai.ai>
2024-04-03 20:01:33 +0800

    Update

--- dev/docker/common/create-ssh-user.sh
+++ dev/docker/common/create-ssh-user.sh
@@ -14,9 +14,8 @@ groupadd -f -g $DOCKER_GROUP_ID $DOCKER_GROUP_NAME
 useradd -s /usr/bin/zsh -d $DOCKER_HOME_DIR -u $DOCKER_USER_ID -g $DOCKER_GROUP_ID $DOCKER_USER_NAME
 usermod -aG sudo $DOCKER_USER_NAME
 echo "$DOCKER_USER_NAME:1" | chpasswd
-#chown -R $DOCKER_USER_NAME:$DOCKER_GROUP_NAME $DOCKER_HOME_DIR
 
-runuser -u $DOCKER_USER_NAME -- $DOCKER_HOME_DIR/miniconda3/bin/conda init zsh
+#runuser -u $DOCKER_USER_NAME -- $DOCKER_HOME_DIR/miniconda3/bin/conda init zsh
 
 # start ssh server
 runuser -u $DOCKER_USER_NAME -- ssh-keygen -A
